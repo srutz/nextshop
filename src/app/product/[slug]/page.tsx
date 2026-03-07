84e51270d3777d7763127569077a351288ea1186
stepan.rutz <stepan.rutz@gmx.de>
2026-03-07 20:57:31 +0100

    more

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation"
 import type { Product } from "@/components/ProductCard"
 import { ProductDetails } from "@/components/ProductDetails"
 
@@ -10,17 +11,28 @@ export async function generateStaticParams() {
 }
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
-  const url = `https://dummyjson.com/products/${slug}`
-
-  console.time("fetch product details for slug: " + slug)
-  const res = await fetch(url, {
-    //cache: "no-store",
-  })
-  console.timeEnd("fetch product details for slug: " + slug)
-
-  const product = await res.json() as Product
+  const product = await getProductBySlut(slug)
+  if (!product) {
+    notFound()
+  }
 
   return (
     <ProductDetails product={product} />
   )
 }
+
+async function getProductBySlut(slug: string): Promise<Product | null> {
+  const url = `https://dummyjson.com/products/${slug}`
+  console.time("fetch product details for slug: " + slug)
+  const res = await fetch(url, {
+    //cache: "no-store",
+  })
+  console.timeEnd("fetch product details for slug: " + slug)
+  if (!res.ok || res.status === 404) {
+    return null
+  }
+  const product = await res.json() as Product
+  return product
+}
+
+
--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Menubar() {
   return (
-    <div className="w-full shrink-0 h-10 bg-blue-500 text-white flex gap-4 items-center px-4">
+    <div className="w-full shrink-0 h-10 bg-gray-800 text-white flex gap-4 items-center px-4">
       THE SHOP
       <Link href={"/"} >Home</Link>
       <Link href={"/product"} >Products</Link>
