678ed49e8f22388b5b84b696cb29eb02bc3b447c
stepan.rutz <stepan.rutz@gmx.de>
2026-03-07 20:43:52 +0100

    more

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import type { Product } from "@/components/ProductCard"
 import { ProductDetails } from "@/components/ProductDetails"
 
+//export const dynamic = 'force-dynamic'
+
 export async function generateStaticParams() {
   return Array.from({ length: 194 }, (_, i) => ({
     slug: (i + 1).toString(),
@@ -8,9 +10,14 @@ export async function generateStaticParams() {
 }
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
-  console.log(">> render product page for slug: ", slug)
+  const url = `https://dummyjson.com/products/${slug}`
 
-  const res = await fetch("https://dummyjson.com/products/" + slug)
+  console.time("fetch product details for slug: " + slug)
+  const res = await fetch(url, {
+    //cache: "no-store",
+  })
+  console.timeEnd("fetch product details for slug: " + slug)
+
   const product = await res.json() as Product
 
   return (
